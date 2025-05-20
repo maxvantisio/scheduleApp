e1dea0984a8384bfea8f2c78bc91c3df0ee0392d
maxvantisio <brownmh3@appstate.edu>
2025-05-19 21:50:13 -0400

    temp layout. will change later

--- renderer.js
+++ renderer.js
@@ -1,10 +1,10 @@
 'use strict';
-
 document.addEventListener('DOMContentLoaded', () => {
 let date = new Date();
 let currYear = date.getFullYear();
 let currMonth = date.getMonth();
 let currDay = date.getDate();
+const realMonth = currMonth; //maybe stop currDay from lighting up when looking at other months
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let leapYear = () => {
     if (currYear % 100 === 0 ? currYear % 400 === 0 : currYear % 4 === 0)
@@ -19,7 +19,7 @@ let leapYear = () => {
 let kindOfMonth = () => {
     if (months[currMonth] != "February")
     {
-        if (months[currMonth] == "April" || months[currMonth] == "September" || months[currMonth] == "June" || months[currMonth] == "November")
+        if (["April", "September", "June", "November"].includes(months[currMonth]))
         {
             return 30;
         }
@@ -46,11 +46,83 @@ function renderHeader() {
     month.appendChild(year);
 }
 //document.getElementById('year').textContent = `${year}`
+document.querySelector('.prev').addEventListener('click', () => {
+    if (currMonth === 0)
+    {
+        currYear -= 1;
+        currMonth = 11;
+    }
+    else
+    {
+        currMonth -= 1;
+    }
+    renderHeader();
+    showDates();
+})
 
+document.querySelector('.next').addEventListener('click', () => {
+    if (currMonth === 11)
+    {
+        currYear += 1;
+        currMonth = 0;
+    }
+    else
+    {
+        currMonth += 1;
+    }
+    renderHeader();
+    showDates();
+})
 
-
+//Render body of calendar
 function showDates() {
     let datesStart = document.createElement('ul');
+
+    //Get # of days in the current month
+    let daysInMonth = (currMonth === 1) ? (leapYear() === "leap" ? 29 : 28) : kindOfMonth();
+
+    const firstDayOfMonth = new Date(currYear, currMonth, 1).getDay();
+
+    //Get days of pevious month
+    const prevMonth = (currMonth === 0) ? 11 : currMonth - 1;
+    const prevYear = (currMonth === 0) ? currYear - 1 : currYear;
+    const numOfDaysPrev = new Date(prevYear, prevMonth + 1, 0).getDate();
+
+    //Render previous month
+    for (let i = firstDayOfMonth - 1; i >= 0; i--)
+    {
+        const day = document.createElement('li');
+        day.textContent = numOfDaysPrev - i;
+        day.classList.add('otherMonths');
+        datesStart.appendChild(day);
+    }
+
+    //Render current month
+    for (let i = 1; i <= daysInMonth; i++)
+    {
+        const day = document.createElement('li');
+        day.textContent = i;
+        if (i === currDay && currMonth === realMonth)
+        {
+            day.id = 'active';
+        }
+        datesStart.appendChild(day);
+    }
+
+    //Render next month
+    const totalCells = firstDayOfMonth + daysInMonth;
+    const trailingCells = 7 - (totalCells % 7);
+    if (trailingCells < 7)
+    {
+        for (let i = 1; i <= trailingCells; i++)
+        {
+            const day = document.createElement('li');
+            day.textContent = i;
+            day.classList.add('otherMonths');
+            datesStart.appendChild(day);
+        }
+    }
+    /*
     if (currMonth == "February")
     {
         if(leapYear() == "noLeap")
@@ -79,7 +151,12 @@ function showDates() {
         } 
         datesStart.appendChild(newDate);
     }
-    document.querySelector('.dates').appendChild(datesStart);
+    //document.querySelector('.dates').innerHTML = '';
+    //document.querySelector('.dates').appendChild(datesStart);
+    */
+    let layout = document.getElementById('dates');
+    layout.innerHTML = '';
+    layout.appendChild(datesStart);
 }
 
 function renderCalendar()
